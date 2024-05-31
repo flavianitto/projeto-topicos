672bc8deb3b27dc593d8c56cab28e781af2fcb08
flavianitto <flavia.nnitto@gmail.com>
2024-05-31 00:52:20 -0300

    add telas

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
+
+import Inicial from './Screens/Inicial';
+
+// const Stack = createNativeStackNavigator()
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#FB8929',
+    accent: '#1B5400',
+    background: '#FDE6DB',
+    text: '#1A1C1E',
+  },
+  fontWeight: {
+    bold: 'bold',
+    regular: 'regular'
+  },
+  fontSizes: {
+    normal: 14,
+    large: 32,
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <Inicial/>
+      {/* <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Inicial" component={Inicial} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer> */}
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
